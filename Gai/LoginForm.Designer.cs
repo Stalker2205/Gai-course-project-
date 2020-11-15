49844dea970ff62654e58d286a4f21d4f231cdfa
Nik <36083065+Stalker2205@users.noreply.github.com>
2020-11-15 20:58:58 +0300

    1

---
+++ Gai/LoginForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Gai
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.databasegaiDataSet = new Gai.DatabasegaiDataSet();
+            this.сотрудникиBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.сотрудникиTableAdapter = new Gai.DatabasegaiDataSetTableAdapters.СотрудникиTableAdapter();
+            this.tableAdapterManager = new Gai.DatabasegaiDataSetTableAdapters.TableAdapterManager();
+            ((System.ComponentModel.ISupportInitialize)(this.databasegaiDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.сотрудникиBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // databasegaiDataSet
+            // 
+            this.databasegaiDataSet.DataSetName = "DatabasegaiDataSet";
+            this.databasegaiDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // сотрудникиBindingSource
+            // 
+            this.сотрудникиBindingSource.DataMember = "Сотрудники";
+            this.сотрудникиBindingSource.DataSource = this.databasegaiDataSet;
+            // 
+            // сотрудникиTableAdapter
+            // 
+            this.сотрудникиTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.UpdateOrder = Gai.DatabasegaiDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            this.tableAdapterManager.АвтоTableAdapter = null;
+            this.tableAdapterManager.ВодителиTableAdapter = null;
+            this.tableAdapterManager.ДТПTableAdapter = null;
+            this.tableAdapterManager.КатегорииTableAdapter = null;
+            this.tableAdapterManager.ПраваTableAdapter = null;
+            this.tableAdapterManager.ПТСTableAdapter = null;
+            this.tableAdapterManager.РапортыTableAdapter = null;
+            this.tableAdapterManager.СотрудникиTableAdapter = this.сотрудникиTableAdapter;
+            this.tableAdapterManager.СтраховкаTableAdapter = null;
+            this.tableAdapterManager.СтсTableAdapter = null;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.databasegaiDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.сотрудникиBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DatabasegaiDataSet databasegaiDataSet;
+        private System.Windows.Forms.BindingSource сотрудникиBindingSource;
+        private DatabasegaiDataSetTableAdapters.СотрудникиTableAdapter сотрудникиTableAdapter;
+        private DatabasegaiDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+    }
+}
---
+++ Gai/LoginForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gai
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void сотрудникиBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.сотрудникиBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.databasegaiDataSet);
+
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "databasegaiDataSet.Сотрудники". При необходимости она может быть перемещена или удалена.
+            this.сотрудникиTableAdapter.Fill(this.databasegaiDataSet.Сотрудники);
+
+        }
+    }
+}
